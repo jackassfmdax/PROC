4a10fb771ad79891ad28e6db2079a2dd0f90e892
Den Bogomolov <jackass.fm@bk.ru>
2017-05-20 15:40:10 +0700

    newclass

--- array.cpp
+++ array.cpp
@@ -3,7 +3,7 @@
 #include "array.h"
 
 using namespace std;
-//ввод параметров диагональной матрицы из файла
+
 void ReadDiagonal(diagonal_ar &d_ar, ifstream &ifst)
 {
 	ifst >> d_ar.count;
@@ -11,7 +11,7 @@ void ReadDiagonal(diagonal_ar &d_ar, ifstream &ifst)
 	for (int i = 0; i < d_ar.count; i++)
 		ifst >> d_ar.ar_d[i];
 }
-//вывод параметров диагональной матрицы в поток
+
 void WriteDiagonal(diagonal_ar &d_ar, ofstream &ofst)
 {
 	ofst << "It is Diagonal Matrix: count of elements = " << d_ar.count << endl << "Matrix:" << endl;
@@ -25,7 +25,7 @@ void WriteDiagonal(diagonal_ar &d_ar, ofstream &ofst)
 		ofst << endl;
 	}
 }
-//ввод параметров обычной матрицы из файла
+
 void ReadUsual(usual_ar &us_ar, ifstream &ifst)
 {
 	ifst >> us_ar.count;
@@ -36,7 +36,7 @@ void ReadUsual(usual_ar &us_ar, ifstream &ifst)
 		for (int j = 0; j < us_ar.count; j++)
 			ifst >> us_ar.ar_us[i][j];
 }
-//вывод параметров обычной матрицы в поток
+
 void WriteUsual(usual_ar &us_ar, ofstream &ofst)
 {
 	ofst << "It is Usual Matrix: count of elements = " << us_ar.count << endl << "Matrix:" << endl;
@@ -47,7 +47,34 @@ void WriteUsual(usual_ar &us_ar, ofstream &ofst)
 		ofst << endl;
 	}
 }
-//ввод параметров матрицы из файла
+
+void ReadTriangle(triangle_ar &tr_ar, ifstream &ifst)
+{
+	ifst >> tr_ar.count;
+	tr_ar.ar_tr = new int*[tr_ar.count];
+	for (int i = 0; i < tr_ar.count; i++)
+		tr_ar.ar_tr[i] = new int[tr_ar.count];
+	for (int i = 0; i < tr_ar.count; i++)
+		for (int j = 0; j < tr_ar.count; j++)
+			if (i >= j)
+				ifst >> tr_ar.ar_tr[i][j];
+}
+
+void WriteTriangle(triangle_ar &tr_ar, ofstream &ofst)
+{
+	ofst << "It is Triangle Matrix: count of elements = " << tr_ar.count << endl << "Matrix:" << endl;
+	for (int i = 0; i < tr_ar.count; i++)
+	{
+		for (int j = 0; j < tr_ar.count; j++)
+			if (i >= j)
+				ofst << tr_ar.ar_tr[i][j] << '\t';
+			else
+				ofst << "0\t";
+		ofst << endl;
+	}
+}
+
+
 arrays* ReadArray(ifstream& ifst)
 {
 	arrays* ar = new arrays;
@@ -63,11 +90,15 @@ arrays* ReadArray(ifstream& ifst)
 		ar->k = arrays::key::Usual;
 		ReadUsual(ar->us, ifst);
 		return ar;
+	case 3:
+		ar->k = arrays::key::Triangle;
+		ReadTriangle(ar->tr, ifst);
+		return ar;
 	default:
 		return NULL;
 	}
 }
-//вывод параметров текущей матрицы в поток
+
 void WriteArray(arrays &write_ar, ofstream &ofst)
 {
 	switch (write_ar.k)
@@ -77,6 +108,9 @@ void WriteArray(arrays &write_ar, ofstream &ofst)
 		break;
 	case arrays::key::Usual:
 		WriteUsual(write_ar.us, ofst);
+		break;
+	case arrays::key::Triangle:
+		WriteTriangle(write_ar.tr, ofst);
 		break;
 	default:
 		ofst << "Incorrect array!" << endl;
--- array.h
+++ array.h
@@ -14,14 +14,21 @@ struct diagonal_ar
 	int* ar_d;
 };
 
+struct triangle_ar
+{
+	int count;
+	int** ar_tr;
+};
+
 struct arrays
 {
-	enum key { Diagonal, Usual };
+	enum key { Diagonal, Usual, Triangle };
 	key k;
 	int count;
 	union
 	{
 		diagonal_ar d;
 		usual_ar us;
+		triangle_ar tr;
 	};
 };
